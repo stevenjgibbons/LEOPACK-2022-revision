eaa92418fef7a74089e7af9f804b654f7f750dc9
Steven J. Gibbons <74380921+stevenjgibbons@users.noreply.github.com>
2022-10-16 15:04:12 +0200

    Add files via upload

---
+++ clean_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+for dir in \
+	LEOPACK_linons1
+do
+    cd $dir
+    make clean
+    cd ..
+done
+#
---
+++ make_all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Compile linons1
+#
+cd LEOPACK_linons1
+make linons1
+cd ..
+#
