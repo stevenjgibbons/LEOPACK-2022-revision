5523d9e3b054ca1c638845b1bfd33da4226b2fed
Steven J. Gibbons <74380921+stevenjgibbons@users.noreply.github.com>
2022-10-18 15:12:33 +0200

    Add files via upload

---
+++ bin/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+for file in \
+	arrows_const_r3    \
+	arrows_z_eq_merid4    \
+	cic_arrows_z_eq_merid4    \
+	cicm_arrows_z_eq_merid4    \
+	continent_arrows_const_r3    \
+	continent_full_sphere_plot    \
+	cutout_sphere_plot    \
+	full_sphere_plot    \
+	ps_2plot_z_eq_merid2    \
+	shc_sphere_plot
+do
+    if test -r $file
+    then
+        rm $file
+    fi
+done
