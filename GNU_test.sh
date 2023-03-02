789868822936560e2c4caf2a01a4c9e0d229e681
Steven J. Gibbons <74380921+stevenjgibbons@users.noreply.github.com>
2023-03-02 21:51:02 +0100

    Add files via upload

---
+++ GNU_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Steve Gibbons 2023/03/02
+# Script obtained from page
+# https://forum.mmm.ucar.edu/threads/how-to-fix-error-rank-mismatch-between-actual-argument-at-1-and-actual-argument-at-2.11998/#post-27521
+#
+
+#IF statement for GNU compiler issue
+export GCC_VERSION=$(/usr/bin/gcc -dumpfullversion | awk '{print$1}')
+export GFORTRAN_VERSION=$(/usr/bin/gfortran -dumpfullversion | awk '{print$1}')
+export GPLUSPLUS_VERSION=$(/usr/bin/g++ -dumpfullversion | awk '{print$1}')
+
+export GCC_VERSION_MAJOR_VERSION=$(echo $GCC_VERSION | awk -F. '{print $1}')
+export GFORTRAN_VERSION_MAJOR_VERSION=$(echo $GFORTRAN_VERSION | awk -F. '{print $1}')
+export GPLUSPLUS_VERSION_MAJOR_VERSION=$(echo $GPLUSPLUS_VERSION | awk -F. '{print $1}')
+
+export version_10="10"
+
+echo "##########################################"
+if [ $GCC_VERSION_MAJOR_VERSION -ge $version_10 ] || [ $GFORTRAN_VERSION_MAJOR_VERSION -ge $version_10 ] || [ $GPLUSPLUS_VERSION_MAJOR_VERSION -ge $version_10 ]
+then
+  export fallow_argument=-fallow-argument-mismatch
+  export boz_argument=-fallow-invalid-boz
+  echo "Comment out the following lines in Makefiles"
+  echo "OPTIM = -O3                                 "
+else
+  export fallow_argument=
+  export boz_argument=
+  echo "Comment out the following lines in Makefiles"
+  echo "OPTIM = -O3 -fallow-argument-mismatch       "
+fi
+
+
+export FFLAGS=$fallow_argument
+export FCFLAGS=$fallow_argument
+
+
+echo "FFLAGS = $FFLAGS"
+echo "FCFLAGS = $FCFLAGS"
+echo "##########################################"
+
+
